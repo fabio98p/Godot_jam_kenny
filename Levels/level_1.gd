a58ac0ff364db47afb1c05a5c642b1f2b96b51bf
Fabio Parisi <parisifabio98@gmail.com>
2025-07-19 17:44:59 +0200

    fix movement

--- Levels/level_1.gd
+++ Levels/level_1.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 @onready var drop: Label = $Drop
 @onready var timer: Node2D = $Timer
+@onready var player_ship: CharacterBody2D = $PlayerShip
 
 func _ready() -> void:
 	timer.start_timer()
+	player_ship.can_move = true
 
 func _process(delta: float) -> void:
 	drop.text = str( GC.collectedDrop )
